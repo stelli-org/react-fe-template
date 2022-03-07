893d4aa4424d17c7311607356b2c960cf33bb54f
Samuel Silva <samuelsilvawb@gmail.com>
2022-03-07 17:45:44 +0000

    chore: Install chakra ui dependencies and insert provider on the top level component
    closes #2

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,11 @@
+import { ChakraProvider, Heading } from "@chakra-ui/react";
+
 function App({ text }: { text?: string }) {
-  return <h1>{text}</h1>;
+  return (
+    <ChakraProvider>
+      <Heading as="h1">{text}</Heading>
+    </ChakraProvider>
+  );
 }
 
 export default App;
